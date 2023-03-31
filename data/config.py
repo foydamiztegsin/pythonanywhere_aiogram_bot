635d8332c1cd76c1d8e432a8a19e0dfe5a243c92
foydamiztegsin <121927950+foydamiztegsin@users.noreply.github.com>
2023-03-31 15:24:58 +0500

    Update config.py

--- data/config.py
+++ data/config.py
@@ -1,11 +1,13 @@
+                                                                          
 from dotenv import load_dotenv
 import os
+import json
 
 
 load_dotenv()
 
-# .env fayl ichidan quyidagilarni o'qiymiz
+
+
 PROXY_URL = os.environ.get("PROXY_URL")  # Bot uchun proxy
 BOT_TOKEN = os.environ.get("BOT_TOKEN") # Bot token
-ADMINS = os.environ.get("ADMINS")  # adminlar ro'yxati
-
+ADMINS = json.loads(os.environ["ADMINS"]) # Adminlar 
